6423c48149aac194a205a341ecad0aa236f0671f
Gustas Z <gustas.zaparackas.001@gmail.com>
2026-01-07 22:54:56 +0200

    The final version?

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - 1 - i; j++) {
                 if (students[j].compareTo(students[j + 1]) > 0) {
-                    // swap elements
+
                     String temp = students[j];
                     students[j] = students[j + 1];
                     students[j + 1] = temp;
