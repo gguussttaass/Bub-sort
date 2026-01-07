d70f1494a8b2b014a3e0fcd3e3eace05c6d39a07
Gustas Z <gustas.zaparackas.001@gmail.com>
2026-01-07 22:52:21 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+
+    public static void studentsSort(String[] students) {
+        int n = students.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (students[j].compareTo(students[j + 1]) > 0) {
+                    // swap elements
+                    String temp = students[j];
+                    students[j] = students[j + 1];
+                    students[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] students = {
+                "benas",
+                "jonas",
+                "bobas",
+                "martynas"
+        };
+
+        studentsSort(students);
+
+        for (String student : students) {
+            System.out.println(student);
+        }
+    }
+}
